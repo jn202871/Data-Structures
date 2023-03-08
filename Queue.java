13fefecbe98a9871aae0ad7c0c07a8ec9218204d
Jay Nash <jn202871@gmail.com>
2023-03-08 15:30:04 -0500

    Node based queue and stack

---
+++ Queue.java
@@ -0,0 +1,38 @@
+public class Queue {
+    private Node head;
+    private Node tail;
+
+    public Queue(){
+        head = null;
+        tail = null;
+    }
+
+    public Node front(){
+        return head;
+    }
+
+    public Node dequeue(){
+        Node temp = head;
+        head = head.getNext();
+        if (head == null){
+            tail = null;
+        }
+        temp.setNext(null);
+        return temp;
+    }
+
+    public Node enqueue(Node x){
+        if (head == null){
+            head = x;
+            tail = x;
+        } else {
+            tail.setNext(x);
+            tail = tail.getNext();
+        }
+        return x;
+    }
+
+    public boolean isEmptyQueue(){
+        return head == null;
+    }
+}
