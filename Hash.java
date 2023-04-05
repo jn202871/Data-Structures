546913a87a3efcdbdd71604a8b7e55982f8015cb
Jay Nash <jn202871@gmail.com>
2023-04-05 16:48:46 -0400

    Hash Structure

---
+++ Hash.java
@@ -0,0 +1,29 @@
+public class Hash {
+    private List[] table;
+    private int hash;
+    
+    public Hash(){
+        table = new List[7];
+        hash = 7;
+    }
+
+    public Hash insert(Node node){
+        table[node.getKey()%hash].insert(node);
+        return this;
+    }
+
+    public Node lookUp(int key){
+        return table[key%hash].searchReturn(key);
+    }
+
+    public Node delete(int key){
+        return table[key%hash].searchRemove(key);
+    }
+
+    public boolean isEmpty(){
+        for (int i=0; i < 7; i++){
+            if (!table[i].isEmptyList()) return false;
+        }
+        return true;
+    }
+}
